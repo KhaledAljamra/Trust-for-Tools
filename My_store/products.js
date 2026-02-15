5ca19d77d46eba5936aa1189244016f8a9e3a6df
KhaledAljamra <k.aljamraboss@gmail.com>
2026-02-15 19:42:31 +0300

    Delete My_store directory

--- My_store/products.js
+++
@@ -1,9 +0,0 @@
-const products = [
-  { id: 3, type: 'hand',  price: 122, image: 'images/3.png', name: 'kk', desc: 'fdasfasdf' },
-  { id: 4, type: 'power', price: 454, image: 'images/4.png', name: 'ss', desc: 'asdfasdfaf' },
-  { id: 5, type: 'auto',  price: 232, image: 'images/5.png', name: 'gg', desc: 'sadfasdf' },
-  { id: 6, type: 'cone',  price: 5424,image: 'images/6.png', name: 'hh', desc: 'sadfasdf' },
-  { id: 7, type: 'hand',  price: 45,  image: 'images/7.png', name: 'rr', desc: 'asdfasdfaf' },
-  { id: 8, type: 'power', price: 454, image: 'images/8.png', name: 'ff', desc: 'asdfasdfaf' },
-  { id: 9, type: 'auto',  price: 4545,image: 'images/9.png', name: 'ssf',desc: 'asdfasdf' }
-];
